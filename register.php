0be7e712b597236f8a39213944f8b02581827fe1
herrbrunner <mail@danielbrunner.net>
2016-11-08 09:32:04 +0100

    Registrierungsfunktion hinzugefügt

--- register.php
+++ register.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+$pdo = new PDO('mysql:host=localhost;dbname=web_projekt', 'root', '1234');
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 
@@ -7,16 +12,84 @@
 
 <body>
 
+    <?php
+$showForm = true;
+
+if (isset($_GET['register'])) {
+    $error = false;
+    $email = $_POST['email'];
+    $vorname = $_POST['vorname'];
+    $nachname = $_POST['nachname'];
+    $password = $_POST['password'];
+    $password2 = $_POST['password2'];
+
+    /* Überprüfung ob eine gültige E-Mail-Adresse angegeben wurde */
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo 'Fehler: Keine gültige E-Mail-Adresse!';
+        $error = true;
+    }
+
+    /* Überprüfung ob ein Passwort angegeben wurde */
+    if (strlen($password) == 0) {
+        echo 'Fehler: kein Passwort angegeben';
+        $error = true;
+    }
+
+    /* Überprüfung ob Passwörter gleich sind */
+    if ($password != $password2) {
+        echo 'Fehler: Passwörter sind nicht identisch';
+        $error = true;
+    }
+
+    /* Prüfung, ob E-Mail schon registriert ist */
+    if (!$error) {
+        $statement = $pdo->prepare("
+            SELECT * FROM users WHERE email = :email
+        ");
+        $result = $statement->execute(array('email' => $email));
+        $user = $statement->fetch();
+
+        if ($user !== false) {
+            echo 'Fehler: E-Mail wurde schon vergeben';
+            $error = true;
+        }
+    }
+
+    /* keine Fehler, Nutzer kann registriert werden */
+    if (!$error) {
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
+        $statement = $pdo->prepare("
+            INSERT INTO users (email, vorname, nachname, password) VALUES (:email, :vorname, :nachname, :password)
+        ");
+        $result = $statement->execute(array('email' => $email, 'vorname' => $vorname, 'nachname' => $nachname, 'password' => $password_hash));
+
+        if ($result) {
+            echo "Registrierung abgeschlossen";
+            $showForm = false;
+        } else {
+            echo 'Fehler: Beim Abspeichern';
+        }
+    }
+}
+    
 <h1>Registrieren</h1>
 
-<form>
-    E-Mail <input type="text" /><br />
-    Vorname <input type="text" /><br />
-    Nachname <input type="text" /><br />
-    Passwort <input type="password" /><br />
-    Passwort wiederholen <input type="password" /><br />
-    <input type="submit" value="Registrieren"> (keine Funktion: <a href="login.php">zur&uuml;ck</a>)
-</form>
+<?php
+if ($showForm) {
+    ?>
+
+    <form action="?register=1" method="post">
+        E-Mail <input type="text" name="email"/><br/>
+        Vorname <input type="text" name="vorname"/><br/>
+        Nachname <input type="text" name="nachname"/><br/>
+        Passwort <input type="password" name="password"/><br/>
+        Passwort wiederholen <input type="password" name="password2"/><br/>
+        <input type="submit" value="Registrieren"> (keine Funktion: <a href="login.php">zur&uuml;ck</a>)
+    </form>
+
+    <?php
+}
+?>
 
 <div style="position: fixed; bottom: 0;">
 
