9d9b2a27b7afcea91fb4f2f77f3b13967030ab7b
herrbrunner <mail@danielbrunner.net>
2016-11-08 22:23:47 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-$pdo = new PDO('mysql:host=localhost;dbname=web_projekt', 'root', '1234');
+$pdo = new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-db118', 'db118', 'password');
+
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +15,6 @@ $pdo = new PDO('mysql:host=localhost;dbname=web_projekt', 'root', '1234');
 
     <?php
 $showForm = true;
-
 if (isset($_GET['register'])) {
     $error = false;
     $email = $_POST['email'];
@@ -22,25 +22,21 @@ if (isset($_GET['register'])) {
     $nachname = $_POST['nachname'];
     $password = $_POST['password'];
     $password2 = $_POST['password2'];
-
     /* Überprüfung ob eine gültige E-Mail-Adresse angegeben wurde */
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo 'Fehler: Keine gültige E-Mail-Adresse!';
         $error = true;
     }
-
     /* Überprüfung ob ein Passwort angegeben wurde */
     if (strlen($password) == 0) {
         echo 'Fehler: kein Passwort angegeben';
         $error = true;
     }
-
     /* Überprüfung ob Passwörter gleich sind */
     if ($password != $password2) {
         echo 'Fehler: Passwörter sind nicht identisch';
         $error = true;
     }
-
     /* Prüfung, ob E-Mail schon registriert ist */
     if (!$error) {
         $statement = $pdo->prepare("
@@ -48,13 +44,11 @@ if (isset($_GET['register'])) {
         ");
         $result = $statement->execute(array('email' => $email));
         $user = $statement->fetch();
-
         if ($user !== false) {
             echo 'Fehler: E-Mail wurde schon vergeben';
             $error = true;
         }
     }
-
     /* keine Fehler, Nutzer kann registriert werden */
     if (!$error) {
         $password_hash = password_hash($password, PASSWORD_DEFAULT);
@@ -62,18 +56,18 @@ if (isset($_GET['register'])) {
             INSERT INTO users (email, vorname, nachname, password) VALUES (:email, :vorname, :nachname, :password)
         ");
         $result = $statement->execute(array('email' => $email, 'vorname' => $vorname, 'nachname' => $nachname, 'password' => $password_hash));
-
         if ($result) {
             echo "Registrierung abgeschlossen";
             $showForm = false;
+            header('Location: login.php');
         } else {
             echo 'Fehler: Beim Abspeichern';
         }
     }
 }
-    
-<h1>Registrieren</h1>
 
+?>    
+<h1>Registrieren</h1>
 <?php
 if ($showForm) {
     ?>
@@ -92,23 +86,7 @@ if ($showForm) {
 ?>
 
 <div style="position: fixed; bottom: 0;">
-
-    <!--
-        Links zu den einzelnen Seiten
-        Nur zur Übersicht
-    -->
-
-    Impressum<br />
-    <h3>&Uuml;bersicht</h3>
-    <ul style="list-style: none">
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Registrieren</a></li>
-        <li><a href="main.php">&Uuml;bersicht</a></li>
-        <li><a href="settings.php">Profileinstellungen</a> </li>
-        <li><a href="upload.php">Dateien hochladen</a> </li>
-        <li><a href="share.php">Dateien teilen</a> </li>
-
-    </ul>
+&copy; 2016
 </div>
 </body>
 </html>
