82b1900ea103d61f0d58c1b6916e7665df16a6a2
lazarevych <liudmyla.lazarevych@gmail.com>
2017-02-05 21:32:15 +0100

    Update header.php

--- header.php
+++ header.php
@@ -14,6 +14,10 @@ echo "
     <link rel='stylesheet' type='text/css' href='font-awesome-4.7.0/css/font-awesome.css'>
     <link rel='stylesheet' href='css/bootstrap-editable.css'>
     <link rel='stylesheet' type='text/css' href='css/style.css'>
+    <link rel='stylesheet' type='text/css' href='css/layout.css'>
+    <link rel='stylesheet' type='text/css' href='css/skin.css'>
+    
+    
     
     <script src='js/jquery-3.1.1.js'></script>
     <script src='js/bootstrap.js'></script>
