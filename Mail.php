72c82b07f4bf92640c72a71aa08decf4839b1cfd
isabellefrantzen <if015@hdm-stuttgart.de>
2017-02-06 22:18:32 +0100

    ...

--- Mail.php
+++ Mail.php
@@ -9,6 +9,8 @@ session_start();
 include 'header.php';
 include 'includes/functions.php';
 
+//es fehlt noch Bezug zu einem Button
+
 $mailtext = '<html>
 <head>
     <title>Folgende Datei wurde mit dir geteilt:</title>
@@ -16,24 +18,25 @@ $mailtext = '<html>
  
 <body>
  
-<h1>includes/download.inc.php?file=<?php echo $file ?>&dir=<?php echo $dir ?></h1>
+<h1>"/includes/download.inc.php?file=<?php echo $file ?>&dir=<?php echo $dir ?>/"</h1>
  
  
 </body>
 </html>
 ';
 
-$empfaenger = "du@testkarnickel.de"; //Mailadresse
-$absender   = "ich@testkarnickel.de";
-$betreff    = "Mail-Test - HTML-E-Mail mit PHP erstellen";
+$empfaenger = $_POST['empfaenger'];
+$absender   = $_POST['absender'];
+$betreff    = $_POST['betreff'];
 $antwortan  = "ICH@testkarnickel.de";
 
 $header  = "MIME-Version: 1.0\r\n";
 $header .= "Content-type: text/html; charset=iso-8859-1\r\n";
 
+
 $header .= "From: $absender\r\n";
 $header .= "Reply-To: $antwortan\r\n";
-// $header .= "Cc: $cc\r\n";  // falls an CC gesendet werden soll
+
 $header .= "X-Mailer: PHP ". phpversion();
 
 mail( $empfaenger,
