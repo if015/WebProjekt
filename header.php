80b1243cf3345e7141b7411992ab448db8459bf2
herrbrunner <mail@danielbrunner.net>
2017-02-08 10:40:23 +0100

    Drag and Drop

--- header.php
+++ header.php
@@ -9,19 +9,18 @@ echo "
 
 <head>
     <title>Box</title>
+    
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' href='css/bootstrap.css'>
     <link rel='stylesheet' type='text/css' href='font-awesome-4.7.0/css/font-awesome.css'>
-    <link rel='stylesheet' href='css/bootstrap-editable.css'>
+    <link rel='stylesheet' type='text/css' href='css/bootstrap-editable.css'>
     <link rel='stylesheet' type='text/css' href='css/style.css'>
-    <link rel='stylesheet' type='text/css' href='css/layout.css'>
-    <link rel='stylesheet' type='text/css' href='css/skin.css'>
-    
-    
-    
+    <link rel='stylesheet' type='text/css' href='css/basic.css'>
+      
     <script src='js/jquery-3.1.1.js'></script>
     <script src='js/bootstrap.js'></script>
     <script src='js/bootstrap-editable.min.js'></script>
+    <script src='js/dropzone.js'></script>
     <script src='js/functions.js'></script>
     
     
