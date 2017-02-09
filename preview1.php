369c7ac2630da4133e8d8e354d711261244dd5a9
lazarevych <liudmyla.lazarevych@gmail.com>
2017-02-09 23:11:44 +0100

    Update preview1.php

--- preview1.php
+++ preview1.php
@@ -6,9 +6,7 @@
  
 
 <?php
-
 $alldata = scandir('files'); // ordner, wo bei mir dateien liegen
-
 foreach ($alldata as $file) {
 	$info = pathinfo('files'."/".$file); 
 	
@@ -16,19 +14,24 @@ foreach ($alldata as $file) {
  {
 	$bildtypen= array("jpg", "jpeg", "gif", "png");
  	if(in_array($info['extension'],$bildtypen)){
-
  ?>
-            <div class="galerie">
+ 
+ <ul id= "galerie">
+ <li>
+             <div class="galerie">
                 <a href="<?php echo $info['dirname']."/".$info['basename'];?>">
                 <img src="<?php echo $info['dirname']."/".$info['basename'];?>" width="140" alt="Vorschau" /></a> 
                 <span><?php echo $info['filename']; ?> </span>
             </div>
+            </li>
  <?php 
  } else { ?>
-            <div class="file">
-             <a href="<?php echo $info['dirname']."/".$info['basename'];?>">&raquo<?php echo $info['filename']; ?></a> (<?php echo $dateiinfo['extension']; ?> 
+ <li>
+            <div class="document">
+             <a href="<?php echo $info['dirname']."/".$info['basename'];?>">&raquo<?php echo $info['filename']; ?></a> (<?php echo $info['extension']; ?> 
             </div>
             <?php } ?>
+            </li>
 <?php
  };
  };
@@ -38,8 +41,5 @@ foreach ($alldata as $file) {
 
 
 
-
-
 </body>
 </html>
-
